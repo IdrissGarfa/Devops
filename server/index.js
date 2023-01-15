6c17772057d1cf07dbefed47289f48d750783b73
Idriss Garfa <idrissgarfa@gmail.com>
2023-01-15 23:11:16 +0100

    SETTING UP Node server

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+const app = express();
+
+mongoose.set('strictQuery', false);
+
+dotenv.config();
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+const PORT = process.env.PORT || 8000;
+
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Conntected to DB successfully 🚀");
+      console.log(`Server running on port: ${PORT} 🕵️`);
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
