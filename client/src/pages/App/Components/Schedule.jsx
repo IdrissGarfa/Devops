b19c9703507c0b04ee4cd909d6dd3cd70a8824f0
Idriss Garfa <idrissgarfa@gmail.com>
2023-01-21 20:13:09 +0100

    App

---
+++ client/src/pages/App/Components/Schedule.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/Io'
+
+const Schedule = () => {
+  return (
+    <div className=''>
+       
+    
+    
+
+    
+    
+    </div>
+  )
+}
+
+export default Schedule
