830cbe65fc4dac1db3270d5bd19720bd9eda9e56
idrissgarfa <idrissgarfa@gmail.com>
2023-02-24 17:57:24 +0100

    Config run scripts

--- client/src/components/Home/Footer.jsx
+++ client/src/components/Home/Footer.jsx
@@ -10,8 +10,8 @@ import { BsInstagram } from "react-icons/bs";
 
 const Footer = () => {
   return (
-      <div className="w-full py-20">
-          <div className="w-[90%] mx-auto grid gap-10 lg:gap-0 lg:justify-items-center grid-cols-1 lg:grid-cols-4 md:grid-cols-2 ">
+      <div className="w-full">
+          <div className="w-[90%] py-20 mx-auto grid gap-10 lg:gap-0 lg:justify-items-center grid-cols-1 lg:grid-cols-4 md:grid-cols-2 ">
               <div className="flex flex-col gap-4 mb-4 md:mb-0 md:order-1 lg:order-1">
                 <div className="flex items-center flex-wrap gap-6">
                   <img src={devopslogo} className='w-20 h-11 lg:w-24 lg:h-12' alt="Devops logo"/>
@@ -24,19 +24,19 @@ const Footer = () => {
               <div className="md:order-3 lg:order-2">
                   <h2 className="text-[#686b7a] font-semibold text-2xl mb-6">Community</h2>
                   <ul className="flex flex-col gap-6">
-                    <li><Link className="text-white text-xl">Our Story</Link></li>
-                    <li><Link className="text-white text-xl">Events</Link></li>
-                    <li><Link className="text-white text-xl">DevOps</Link></li>
-                    <li><Link className="text-white text-xl">Blog</Link></li>
+                    <li><Link className="text-white text-lg md:text-xl">Our Story</Link></li>
+                    <li><Link className="text-white text-lg md:text-xl">Events</Link></li>
+                    <li><Link className="text-white text-lg md:text-xl">DevOps</Link></li>
+                    <li><Link className="text-white text-lg md:text-xl">Blog</Link></li>
                   </ul>
               </div>
               <div className="md:order-4 lg:order-3">
                   <h2 className="text-[#686b7a] font-semibold text-2xl mb-6">Ressources</h2>
                   <ul className="flex flex-col gap-6">
-                    <li><Link className="text-white text-xl">Apps</Link></li>
-                    <li><Link className="text-white text-xl">Developer</Link></li>
-                    <li><Link className="text-white text-xl">Integration</Link></li>
-                    <li><Link className="text-white text-xl">Community</Link></li>
+                    <li><Link className="text-white text-lg md:text-xl">Apps</Link></li>
+                    <li><Link className="text-white text-lg md:text-xl">Developer</Link></li>
+                    <li><Link className="text-white text-lg md:text-xl">Integration</Link></li>
+                    <li><Link className="text-white text-lg md:text-xl">Community</Link></li>
                   </ul>
               </div>
               <div className="md:order-2 lg:order-4">
@@ -44,13 +44,13 @@ const Footer = () => {
                   <ul className="flex flex-col gap-6">
                     <li>
                       <Link className="text-white text-xl flex items-center gap-4">
-                        <MdOutlineEmail className="text-gray-300 text-2xl"/>
+                        <MdOutlineEmail className="text-gray-300 text-lg md:text-xl"/>
                         devopsisima@gmail.com
                       </Link>
                     </li>
                     <li>
                       <Link className="text-white text-xl flex items-center gap-4">
-                        <FiPhoneCall  className="text-gray-300 text-2xl" />
+                        <FiPhoneCall  className="text-gray-300 text-lg md:text-xl" />
                         54 902 901
                       </Link>
                     </li>
@@ -67,7 +67,6 @@ const Footer = () => {
                     </li>
                   </ul>
               </div>
-              
           </div>
       </div>
     )
